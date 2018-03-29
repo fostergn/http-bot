8af83590964268ef787e1ce50d6858b4c5fb1110
David Dotterer <david.dotterer@outlook.com>
2018-03-29 17:23:59 -0400

    add credentials vue and separate settings stuff for working example

---
+++ src/templates/credentials.vue
@@ -0,0 +1,21 @@
+<div id="credentials-http" class="bot-credentials-input">
+    <div class="full-padding gray-bg">
+    <p>Add credentials for your HTTP Request below.</p>
+  </div>
+  <div class="clearfix view-input">
+    <label for="name">Credential Name</label>
+    <input id="name" name="name" type="text" value="my credential name goes here">
+  </div>
+  <div class="clearfix view-input">
+    <label for="scheme">Scheme</label>
+    <select id="scheme" name="scheme" style="width: 400px;">
+      <option value=""></option>
+      <option value="basic" selected>Basic</option>
+      <option value="bearer">Bearer</option>
+    </select>
+  </div>
+  <div class="clearfix view-input">
+    <label for="credentials">Credentials</label>
+    <input id="credentials" name="credentials" type="text" value="super secret credentials">
+  </div>
+</div>
--- src/templates/settings.vue
+++ src/templates/settings.vue
@@ -1,27 +1,9 @@
-<div id="settings-http" class="bot-credentials-input">
+<div id="settings-http" class="bot-settings-input">
     <div class="full-padding gray-bg">
-    <p>Add credentials from your HTTP Request below.</p>
+    <p>HTTP Settings</p>
   </div>
-  <form class="form-modal sixhundred bottom-margin">
-    <div class="clearfix view-input">
-      <label for="uri">URI</label>
-      <input id="uri" name="uri" type="text">
-    </div>
-    <div class="clearfix view-input">
-      <label for="name">Credential Name</label>
-      <input id="name" name="name" type="text">
-    </div>
-    <div class="clearfix view-input">
-      <label for="scheme">Scheme</label>
-      <select id="scheme" name="scheme" style="width: 400px;">
-        <option value=""></option>
-        <option value="basic">Basic</option>
-        <option value="bearer">Bearer</option>
-      </select>
-    </div>
-    <div class="clearfix view-input">
-      <label for="credentials">Credentials</label>
-      <input id="credentials" name="credentials" type="text">
-    </div>
-  </form>
+  <div class="clearfix view-input">
+    <label for="uri">URI</label>
+    <input id="uri" name="uri" type="text" value="www.google.com">
+  </div>
 </div>
